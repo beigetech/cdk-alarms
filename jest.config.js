22106e075b04b75687eeeaaa2916192256cb4cce
Guy Belsham <guy@beigetech.com.au>
2020-09-22 02:07:03 +0000

    Guy/coverage

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,18 @@
 module.exports = {
-  roots: ['<rootDir>/test'],
+	roots: ['<rootDir>/test', '<rootDir>/lib'],
+	collectCoverageFrom: ['lib/*.js', '!lib/index.js'],
   testMatch: ['**/*.test.ts'],
   transform: {
     '^.+\\.tsx?$': 'ts-jest'
+  },
+  coverageReporters: ['text-summary'],	
+  coverageThreshold: {
+    global: {
+	    branches: 100,
+	    functions: 100,
+	    lines: 100,
+	    statetments: 100
+    }
   }
+	
 };
--- test/rds_alarm.test.ts
+++ test/rds_alarm.test.ts
@@ -1,6 +1,6 @@
 import "@aws-cdk/assert/jest";
 import { SynthUtils } from "@aws-cdk/assert";
-import { Stack } from "@aws-cdk/core";
+import { Stack, CfnElement } from "@aws-cdk/core";
 import {
   DatabaseCluster,
   DatabaseClusterEngine,
@@ -13,7 +13,7 @@ import {
   InstanceSize,
   InstanceClass,
 } from "@aws-cdk/aws-ec2";
-import { DBAlarm } from "../lib/rds_alarm";
+import { DatabaseAlarm } from "../lib/rds_alarm";
 
 test("Should generate default alarms for RDS Cluster", () => {
   let stack = new Stack();
@@ -29,7 +29,7 @@ test("Should generate default alarms for RDS Cluster", () => {
     },
   });
 
-  DBAlarm.createClusterAlarms(stack, cluster);
+  DatabaseAlarm.createClusterAlarms(stack, cluster);
 
   expect(stack).toHaveResourceLike("AWS::CloudWatch::Alarm", {
     MetricName: "CPUUtilization",
@@ -56,6 +56,99 @@ test("Should generate default alarms for RDS Cluster", () => {
   });
 });
 
+test("Should custom alarms for RDS Cluster", () => {
+  let stack = new Stack();
+
+  let cluster = new DatabaseCluster(stack, "test-cluster", {
+    engine: DatabaseClusterEngine.AURORA_MYSQL,
+    masterUser: {
+      username: "admin",
+    },
+    instanceProps: {
+      vpc: new Vpc(stack, "test-vpc"),
+      instanceType: InstanceType.of(InstanceClass.T3, InstanceSize.MICRO),
+    },
+  });
+
+  DatabaseAlarm.createClusterAlarms(stack, cluster, {
+    highCpuPct: 80,
+    highCpuEnabled: true,
+    lowMemoryEnabled: false,
+    deadLockEnabled: false,
+    writeLatencyEnabled: false,
+    readLatencyEnabled: false,
+  });
+
+  expect(stack).toHaveResourceLike("AWS::CloudWatch::Alarm", {
+    MetricName: "CPUUtilization",
+    Threshold: 80,
+    Namespace: "AWS/RDS",
+    Period: 60,
+    Statistic: "Average",
+  });
+
+  expect(stack).not.toHaveResourceLike("AWS::CloudWatch::Alarm", {
+    MetricName: "FreeableMemory",
+    Threshold: 100 * 1024 * 1024,
+    Namespace: "AWS/RDS",
+    Period: 60,
+    Statistic: "Average",
+  });
+
+  expect(stack).not.toHaveResourceLike("AWS::CloudWatch::Alarm", {
+    MetricName: "Deadlocks",
+    Threshold: 1,
+    Namespace: "AWS/RDS",
+    Period: 60,
+    Statistic: "Average",
+  });
+});
+
+test("Should create no alarms for RDS Cluster", () => {
+  let stack = new Stack();
+
+  let cluster = new DatabaseCluster(stack, "test-cluster", {
+    engine: DatabaseClusterEngine.AURORA_MYSQL,
+    masterUser: {
+      username: "admin",
+    },
+    instanceProps: {
+      vpc: new Vpc(stack, "test-vpc"),
+      instanceType: InstanceType.of(InstanceClass.T3, InstanceSize.MICRO),
+    },
+  });
+
+  DatabaseAlarm.createClusterAlarms(stack, cluster, {
+    highCpuEnabled: false,
+    lowMemoryEnabled: false,
+    deadLockEnabled: false,
+    writeLatencyEnabled: false,
+    readLatencyEnabled: false,
+  });
+
+  expect(stack).not.toHaveResourceLike("AWS::CloudWatch::Alarm", {});
+});
+
+test("Should create no alarms for RDS Instance", () => {
+  let stack = new Stack();
+  let inst = new DatabaseInstance(stack, "test-instance", {
+    engine: DatabaseInstanceEngine.MYSQL,
+    vpc: new Vpc(stack, "test-vpc"),
+    masterUsername: "admin",
+    instanceType: InstanceType.of(InstanceClass.T3, InstanceSize.MICRO),
+  });
+
+  DatabaseAlarm.createInstanceAlarms(stack, inst, {
+    highCpuEnabled: false,
+    lowMemoryEnabled: false,
+    deadLockEnabled: false,
+    writeLatencyEnabled: false,
+    readLatencyEnabled: false,
+  });
+
+  expect(stack).not.toHaveResourceLike("AWS::CloudWatch::Alarm", {});
+});
+
 test("Should generate default alarms for RDS Instance", () => {
   let stack = new Stack();
   let inst = new DatabaseInstance(stack, "test-instance", {
@@ -65,7 +158,7 @@ test("Should generate default alarms for RDS Instance", () => {
     instanceType: InstanceType.of(InstanceClass.T3, InstanceSize.MICRO),
   });
 
-  DBAlarm.createInstanceAlarms(stack, inst);
+  DatabaseAlarm.createInstanceAlarms(stack, inst);
 
   expect(stack).toHaveResourceLike("AWS::CloudWatch::Alarm", {
     MetricName: "CPUUtilization",
@@ -78,6 +171,184 @@ test("Should generate default alarms for RDS Instance", () => {
   expect(stack).toHaveResourceLike("AWS::CloudWatch::Alarm", {
     MetricName: "FreeableMemory",
     Threshold: 100 * 1024 * 1024,
+    Namespace: "AWS/RDS",
+    Period: 60,
+    Statistic: "Average",
+  });
+});
+
+test("Should generate event subscription for RDS Cluster", () => {
+  let stack = new Stack();
+
+  let cluster = new DatabaseCluster(stack, "test-cluster", {
+    engine: DatabaseClusterEngine.AURORA_MYSQL,
+    masterUser: {
+      username: "admin",
+    },
+    instanceProps: {
+      vpc: new Vpc(stack, "test-vpc"),
+      instanceType: InstanceType.of(InstanceClass.T3, InstanceSize.MICRO),
+    },
+  });
+
+  DatabaseAlarm.subcribeClusterEventsToSlack(stack, "rds-events", cluster, {
+    username: "Alarm Bot",
+    url: "/slack/webhook",
+    channel: "#alerts",
+  });
+
+  expect(stack).toHaveResourceLike("AWS::RDS::EventSubscription", {
+    EventCategories: [
+      "availability",
+      "backup",
+      "configuration change",
+      "creation",
+      "deletion",
+      "failover",
+      "failure",
+      "low storage",
+      "read replica",
+      "recovery",
+    ],
+    SourceIds: [
+      {
+        Ref: stack.getLogicalId(cluster.node.defaultChild as CfnElement),
+      },
+    ],
+  });
+});
+
+test("Should generate custom event subscription for RDS Cluster", () => {
+  let stack = new Stack();
+
+  let cluster = new DatabaseCluster(stack, "test-cluster", {
+    engine: DatabaseClusterEngine.AURORA_MYSQL,
+    masterUser: {
+      username: "admin",
+    },
+    instanceProps: {
+      vpc: new Vpc(stack, "test-vpc"),
+      instanceType: InstanceType.of(InstanceClass.T3, InstanceSize.MICRO),
+    },
+  });
+
+  DatabaseAlarm.subcribeClusterEventsToSlack(
+    stack,
+    "rds-events",
+    cluster,
+    {
+      username: "Alarm Bot",
+      url: "/slack/webhook",
+      channel: "#alerts",
+    },
+    ["availability"]
+  );
+
+  expect(stack).toHaveResourceLike("AWS::RDS::EventSubscription", {
+    EventCategories: ["availability"],
+    SourceIds: [
+      {
+        Ref: stack.getLogicalId(cluster.node.defaultChild as CfnElement),
+      },
+    ],
+  });
+});
+
+test("Should create CPU alarm with default", () => {
+  let stack = new Stack();
+  let inst = new DatabaseInstance(stack, "test-instance", {
+    engine: DatabaseInstanceEngine.MYSQL,
+    vpc: new Vpc(stack, "test-vpc"),
+    masterUsername: "admin",
+    instanceType: InstanceType.of(InstanceClass.T3, InstanceSize.MICRO),
+  });
+
+  DatabaseAlarm.createCpuAlarm(stack, inst);
+
+  expect(stack).toHaveResourceLike("AWS::CloudWatch::Alarm", {
+    MetricName: "CPUUtilization",
+    Threshold: 90,
+    Namespace: "AWS/RDS",
+    Period: 60,
+    Statistic: "Average",
+  });
+});
+
+test("Should create low memory alarm with default", () => {
+  let stack = new Stack();
+  let inst = new DatabaseInstance(stack, "test-instance", {
+    engine: DatabaseInstanceEngine.MYSQL,
+    vpc: new Vpc(stack, "test-vpc"),
+    masterUsername: "admin",
+    instanceType: InstanceType.of(InstanceClass.T3, InstanceSize.MICRO),
+  });
+
+  DatabaseAlarm.createFreeableMemoryAlarm(stack, inst);
+
+  expect(stack).toHaveResourceLike("AWS::CloudWatch::Alarm", {
+    MetricName: "FreeableMemory",
+    Namespace: "AWS/RDS",
+    Period: 60,
+    Statistic: "Average",
+  });
+});
+
+test("Should create write latency alarm with default", () => {
+  let stack = new Stack();
+  let inst = new DatabaseInstance(stack, "test-instance", {
+    engine: DatabaseInstanceEngine.MYSQL,
+    vpc: new Vpc(stack, "test-vpc"),
+    masterUsername: "admin",
+    instanceType: InstanceType.of(InstanceClass.T3, InstanceSize.MICRO),
+  });
+
+  DatabaseAlarm.createWriteLatencyAlarm(stack, inst);
+
+  expect(stack).toHaveResourceLike("AWS::CloudWatch::Alarm", {
+    MetricName: "WriteLatency",
+    Namespace: "AWS/RDS",
+    Period: 60,
+    Statistic: "Average",
+  });
+});
+
+test("Should create read latency alarm with default", () => {
+  let stack = new Stack();
+  let inst = new DatabaseInstance(stack, "test-instance", {
+    engine: DatabaseInstanceEngine.MYSQL,
+    vpc: new Vpc(stack, "test-vpc"),
+    masterUsername: "admin",
+    instanceType: InstanceType.of(InstanceClass.T3, InstanceSize.MICRO),
+  });
+
+  DatabaseAlarm.createReadLatencyAlarm(stack, inst);
+
+  expect(stack).toHaveResourceLike("AWS::CloudWatch::Alarm", {
+    MetricName: "ReadLatency",
+    Namespace: "AWS/RDS",
+    Period: 60,
+    Statistic: "Average",
+  });
+});
+
+test("Should create deadlock alarm with default", () => {
+  let stack = new Stack();
+
+  let cluster = new DatabaseCluster(stack, "test-cluster", {
+    engine: DatabaseClusterEngine.AURORA_MYSQL,
+    masterUser: {
+      username: "admin",
+    },
+    instanceProps: {
+      vpc: new Vpc(stack, "test-vpc"),
+      instanceType: InstanceType.of(InstanceClass.T3, InstanceSize.MICRO),
+    },
+  });
+
+  DatabaseAlarm.createDeadlockAlarm(stack, cluster);
+
+  expect(stack).toHaveResourceLike("AWS::CloudWatch::Alarm", {
+    MetricName: "Deadlocks",
     Namespace: "AWS/RDS",
     Period: 60,
     Statistic: "Average",
